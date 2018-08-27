6070b2b1ac9f118e97a83a53f1a9b5637b8db49d
UstNana <41002698+UstNana@users.noreply.github.com>
2018-08-28 02:16:44 +0300

    Update dif_data.py

--- dif_data.py
+++ dif_data.py
@@ -1,23 +1,24 @@
 #-*- coding: utf8 -*-
 import json
-from pprint import pprint
+
 
 d = {}
 with open("newsafr.json", encoding="utf8") as json_file:
      data_json = json.load(json_file)
      for key, value in data_json.items():
-         d[len(key)] = [key]
          for key_2, value_2 in value.items():
-             d[len(key_2)] = [key_2]
              if key_2 == "channel":
                for key_3, value_3 in value_2.items():
-                 d[len(key_3)] = [key_3]
-                 print(d)
+                 d[len(value_3)] = [value_3]
                  if key_3 == "items":
                      for elements in value_3:
                        for key_4, value_4 in elements.items():
-                           d[len(key_4)] = [key_4]
+                           d[len(value_4)] = [value_4]
 
 
 for key in reversed(sorted(d.keys())):
-    print(key, ":", d[key])
+    if len(d[key]) <= 10:
+        print(key, ":", d[key])
+    else:
+        break
+
